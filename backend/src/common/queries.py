86f6fd831daecb465fef91f8df0b873bf1c4c3c1
Liggerman <liggerman@mail.ru>
2025-09-25 00:58:43 +0300

    Rename table

--- backend/src/common/queries.py
+++ backend/src/common/queries.py
@@ -1,17 +1,11 @@
 TABLE_CREATE = {
-    "card_table": """CREATE TABLE IF NOT EXISTS cards (
+    "cards": """CREATE TABLE IF NOT EXISTS cards (
         id SERIAL PRIMARY KEY,
         color VARCHAR(15),
         set_code VARCHAR(15) NOT NULL,
         set_name VARCHAR(50),
         collector_number INTEGER NOT NULL,
         name VARCHAR(50) NOT NULL,
-        lang VARCHAR(15) NOT NULL,
-        quality VARCHAR(20),
-        foil BOOLEAN,
-        quantity INTEGER NOT NULL DEFAULT 0,
-        price DECIMAL(10,2),
-        owner VARCHAR(50),
         created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
         updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
     );""",
