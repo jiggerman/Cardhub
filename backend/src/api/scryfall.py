869184d5fd0266713df411e9a6d3db7e50ab5046
Liggerman <liggerman@mail.ru>
2025-09-24 19:18:47 +0300

    Create class for scryfall

---
+++ backend/src/api/scryfall.py
@@ -0,0 +1,11 @@
+import requests
+
+class Scryfall:
+    def __int__(self, url="https://api.scryfall.com/"):
+        self.url = url
+
+    def search_card(self):
+        pass
+
+    def parse_defaulte_database(self):
+        pass
