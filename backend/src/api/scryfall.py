cc96648ba84231379598ba206d1a879cfaaa6017
Liggerman <liggerman@mail.ru>
2025-10-27 00:50:03 +0300

    Add card search in scryfall

--- backend/src/api/scryfall.py
+++ backend/src/api/scryfall.py
@@ -1,11 +1,17 @@
 import requests
 
 class Scryfall:
-    def __int__(self, url="https://api.scryfall.com/"):
+    def __init__(self, url="https://api.scryfall.com/"):
         self.url = url
 
-    def search_card(self):
-        pass
+    def search_card(self, card_name):
+        end_point = f"cards/search?q={card_name}"
+        request = requests.get(self.url + end_point)
+
+        if request.status_code == 200:
+            return request.json()
+
+        return []
 
     def parse_defaulte_database(self):
         pass
