837f7e6a0c7351991353c1d55c92b10eaaa103bc
Liggerman <liggerman@mail.ru>
2025-11-09 02:54:47 +0300

    Add run.py

---
+++ backend/src/run.py
@@ -0,0 +1,8 @@
+from backend.src.web_backend.app import create_app
+from backend.src.common.database import Database
+
+app = create_app()
+db = Database()
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
