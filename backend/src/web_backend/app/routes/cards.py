d7094978994408c5e3a046287c4ea3e4173162ff
Liggerman <liggerman@mail.ru>
2025-11-09 02:56:06 +0300

    add hand /cards/search

---
+++ backend/src/web_backend/app/routes/cards.py
@@ -0,0 +1,23 @@
+import os
+import sys
+
+from flask import Blueprint, jsonify, request, current_app
+
+
+from backend.src.common.database import Database
+
+
+bp = Blueprint('cards', __name__)
+
+
+@bp.route('/cards/search/<string:card_name>', methods=['GET'])
+def search_cards(card_name):
+    try:
+        db: Database = current_app.db
+        cards = db.search_card(card_name=card_name)
+        return jsonify(cards)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+
