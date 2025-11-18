1b6026b9d7543534029563fe11e955ec0b7e6daf
Liggerman <liggerman@mail.ru>
2025-11-18 09:47:36 +0300

    Add create app

---
+++ backend/src/web_backend/app/__init__.py
@@ -0,0 +1,43 @@
+import os
+from dotenv import load_dotenv
+
+from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_cors import CORS
+
+from backend.src.db import Database
+from flask_jwt_extended import JWTManager
+from itsdangerous import URLSafeTimedSerializer
+
+
+def create_app():
+    load_dotenv()
+
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+    CORS(app, resources={
+        r"/cards/*": {
+            "origins": ["http://localhost:3000"],
+            "methods": ["GET", "POST"],
+            "allow_headers": ["Content-Type"]
+        },
+        r"/auth/*": {
+            "origins": ["http://localhost:3000"],
+            "methods": ["GET", "POST"],
+            "allow_headers": ["Content-Type", "Authorization", "X-Requested-With"]
+        }
+    })
+
+    from backend.src.web_backend.app.routes import cards
+    from backend.src.web_backend.app.routes import base_user
+
+    app.db = Database()
+    app.bcrypt = Bcrypt(app)
+    app.jwt = JWTManager(app)
+    app.serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+
+    app.register_blueprint(cards.bp)
+    app.register_blueprint(base_user.bp)
+
+    return app
