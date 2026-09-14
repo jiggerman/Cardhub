7d31d97aa43572f4ca53a838d18adebdbbadcaa4
Liggerman <liggerman@mail.ru>
2026-09-14 18:49:50 +0300

    Remove Flask backend

--- backend/src/run.py
+++
@@ -1,8 +0,0 @@
-from backend.src.web_backend.app import create_app
-from backend.src.db import Database
-
-app = create_app()
-db = Database()
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
--- backend/src/web_backend/app/routes/__init__.py
+++
@@ -1,4 +0,0 @@
-from .cards import bp as cards_dp
-from .base_user import bp as base_user_dp
-
-__all__ = ['cards_dp', 'base_user_dp']
